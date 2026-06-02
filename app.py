4ba89ae1522e836fa5f4cd146db2c8757697778d
Nikolaos Rouskas <niro497@email.com>
2026-06-02 16:40:13 +0300

    top-10-users added

--- app.py
+++ app.py
@@ -59,10 +59,40 @@ def register():
 def register_success():
     return render_template("register-success.html")
 
-@app.route("/users")
-def users():
-    
-    return render_template("users.html")
+@app.route("/top-users") #method = "Get"
+def top_users():
+    cursor = conn.cursor()
+
+    #υποθέτουμε οτι κανένας δεν έχει κανει comment στην φωτογραφία
+    #του αφού το έχουμε τσεκάρει μέσω trigger
+    #αν ολοι οι χρήστες εχουν βαθμολογία = 0 φαίνονται 10 τυχαίοι
+    cursor.execute("""SELECT u.user_id, u.first_name, u.last_name, 
+                          COALESCE(photo_counts.num_photos, 0) + COALESCE(comment_counts.num_comments, 0) AS contribution_score
+                      FROM users u
+
+                      LEFT JOIN (
+                          SELECT a.user_id, COUNT(p.photo_id) AS num_photos
+                          FROM albums a
+                          JOIN photos p ON a.album_id = p.album_id
+                          GROUP BY a.user_id
+                      ) AS photo_counts ON u.user_id = photo_counts.user_id
+
+                      LEFT JOIN (
+                          SELECT c.user_id, COUNT(c.comment_id) AS num_comments
+                          FROM comments c
+                          JOIN photos p ON c.photo_id = p.photo_id
+                          JOIN albums a ON p.album_id = a.album_id
+                          WHERE c.user_id != a.user_id  -- Εξασφαλίζει ότι η φωτογραφία ανήκει σε άλλον
+                          GROUP BY c.user_id
+                      ) AS comment_counts ON u.user_id = comment_counts.user_id
+                      ORDER BY contribution_score DESC
+                      LIMIT 10;
+                    """)
+
+    top_ten_users = cursor.fetchall()
+
+    cursor.close()
+    return render_template("top-users.html", top_users=top_ten_users)
 
 if __name__ == "__main__":
     app.run()
